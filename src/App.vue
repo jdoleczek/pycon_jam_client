36bcfb111b3f84ee67069a596786a0988d3baecf
Jan Doleczek <jan@doleczek.pl>
2018-08-24 01:55:30 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <!--div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/ -->
+    <h1 class="mt-5">
+      <center>
+        Lets play a game
+      </center>
+    </h1>
+    <center>
+      <img src="maska.jpg">
+    </center>
+
+    <center class="mt-5">
+      <b-button variant="success" size="lg">Play</b-button>
+    </center>
+  </div>
+</template>
+
+<style lang="scss">
+</style>
